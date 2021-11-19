0bbb74b1cf22bb8ca26f599d9284d11b73cb8921
Kristalev Danil <kristalev2@gmail.com>
2021-11-19 15:39:43 +0700

    Add content to BookingScreen

--- Example/Example/Examples/BookingScreen/BookingScreen.swift
+++ Example/Example/Examples/BookingScreen/BookingScreen.swift
@@ -11,7 +11,7 @@ import ScalingHeaderView
 
 struct BookingScreen: View {
     
-    @ObservedObject private var viewModel = ProfileScreenViewModel()
+    @ObservedObject private var viewModel = BookingScreenViewModel()
     @Environment(\.presentationMode) var presentationMode
     
     
@@ -22,6 +22,7 @@ struct BookingScreen: View {
                    
                 }
             } content: {
+                bookingContentView
             }
             .allowsHeaderCollapse(false)
             .allowsHeaderScale(true)
@@ -38,9 +39,9 @@ struct BookingScreen: View {
                     .buttonStyle(CircleButtonStyle(imageName: "arrow.backward"))
                     .padding(.leading, 16)
                 Spacer()
-                Button("", action: { print("Info") })
+                Button("", action: { print("Share") })
                     .buttonStyle(CircleButtonStyle(imageName: "arrowshape.turn.up.forward.fill"))
-                Button("", action: { print("Info") })
+                Button("", action: { print("Like") })
                     .buttonStyle(CircleButtonStyle(foreground: Color.hex("#F62154"), imageName: "heart.fill"))
                     .padding(.trailing, 16)
             }
@@ -48,6 +49,122 @@ struct BookingScreen: View {
         }
         .padding(.top, 45)
         .ignoresSafeArea()
+    }
+    
+    private var bookingContentView: some View {
+        VStack {
+            contentHeader
+            details
+        }
+        .padding(.top, 40)
+        .padding(.horizontal, 24)
+    }
+    
+    private var contentHeader: some View {
+        VStack(spacing: 0) {
+            HStack {
+                placeName
+                Spacer()
+                stars
+            }
+            address
+        }
+        
+    }
+    
+    private var placeName: some View {
+        Text(viewModel.placeName)
+            .foregroundColor(Color.hex("#0C0C0C"))
+            .font(.custom("Circe-Bold", size: 24))
+    }
+    
+    private var stars: some View {
+        HStack(alignment: .center , spacing: 8) {
+            Image("Star")
+                .offset(y: -3)
+            Text(String(format: "%.1f", viewModel.stars))
+                .foregroundColor(Color.hex("#FFAC0C"))
+                .font(.custom("Circe-Bold", size: 18))
+        }
+    }
+    
+    private var address: some View {
+        HStack(spacing: 6.5) {
+            Image(systemName:  "mappin")
+                .foregroundColor(Color.hex("#1874E0"))
+                .frame(width: 11, height: 14)
+            Text(viewModel.address)
+                .foregroundColor(Color.hex("#1874E0"))
+                .font(.custom("Circe", size: 16))
+            Spacer()
+
+        }
+    }
+    
+    private var details: some View {
+        VStack(spacing: -2) {
+            detailsHeader
+            roomDesription
+            badges
+            description
+        }
+        .padding(.top, 30)
+    }
+    
+    private var detailsHeader: some View {
+        HStack {
+            Text("Details")
+                .foregroundColor(Color.hex("#0C0C0C"))
+                .font(.custom("Circe-Bold", size: 18))
+            Spacer()
+        }
+    }
+    
+    private var roomDesription: some View {
+        HStack {
+            Text(viewModel.roomDesription)
+                .foregroundColor(Color.hex("#0C0C0C").opacity(0.8))
+                .font(.custom("Circe", size: 16))
+            Spacer()
+            
+        }
+    }
+    
+    private var badges: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(viewModel.badges.indices) { index in
+                    let (imageName, title) = viewModel.badges[index]
+                    badgeView(image: imageName, title: title)
+                }
+            }
+        }
+        .padding(.top, 20)
+    }
+    
+    private func badgeView(image: String, title: String) -> some View {
+        RoundedRectangle(cornerRadius: 16)
+            .fill(Color.black.opacity(0.04))
+            .overlay(
+                VStack {
+                    Image(systemName: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 25, height: 21)
+                    Text(title)
+                        .foregroundColor(Color.hex("#0C0C0C").opacity(0.8))
+                        .font(.custom("Circe", size: 12))
+                }
+                    .padding(.top, 21)
+            )
+            .frame(width: 80, height: 80)
+    }
+    
+    private var description: some View {
+        Text(viewModel.description)
+            .foregroundColor(Color.hex("#0C0C0C").opacity(0.8))
+            .font(.custom("Circe", size: 16))
+            .padding(.top, 32)
     }
 }
 
--- Example/Example/Examples/BookingScreen/BookingScreenViewModel.swift
+++ Example/Example/Examples/BookingScreen/BookingScreenViewModel.swift
@@ -9,5 +9,20 @@
 import Foundation
 
 class BookingScreenViewModel: ObservableObject {
+    @Published var placeName: String = "Capo Bay Hotel"
+    @Published var stars: Double = 4.9
+    @Published var address: String = "2, Iasonos Street, 5311 Pritaras, Cyprus"
+    @Published var roomDesription: String = "2 beds • 2 bedroom • 1 kitchen • 1 bath"
+    @Published var badges: [(String, String)] = [("wifi", "Wi-fi"),("tv", "TV"), ("creditcard", "Card"), ("fork.knife", "Breakfast"), ("car","Parking")]
+    
+    @Published var description: String = """
+    The Capo Bay Hotel in Protaras, Cyprus is exceptionally located in the heart of Protaras. This beach hotel in Protaras features gardens with running waters and fish ponds lead down to the waterfront and the Blue Flag Fig Tree Bay.
+
+    Capo Bay guest rooms are equipped with air conditioning, satellite TV and hot drink facilities. Free WiFi is available throughout. Several rooms face the Mediterranean Sea.
+
+    Buffet breakfast is served every morning, and the hotel’s restaurant provides far-reaching sea views. Lunch and dinner are served at the Koi lounge bar, at the a la carte restaurant or the pool bar when the weather is warm.
+
+    Ideal for relaxing holidays, the Capo Bay has a spa center which includes a sauna, hot tub, steam bath, hammam, Vichy shower, a fully equipped gym and a 14 m heated indoor lap pool. Recreation facilities include a modern fitness center and a scuba diving school.
+    """
     
 }
