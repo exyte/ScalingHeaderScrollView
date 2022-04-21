90f62733ead3bd26e9e6f798792613360f7d89ad
AlisaMylnikova <alisa.mylnikova@exyte.com>
2022-04-21 12:53:37 +0700

    Update SPM
    Update SPM
    
    Update SPM
    
    Update SPM
    
    Update SPM
    
    Update SPM
    
    Update SPM
    
    Update SPM

--- Package.swift
+++ Package.swift
@@ -16,14 +16,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ScalingHeaderScrollView",
-            dependencies: [],
+            dependencies: ["Introspect"],
             path: "Source"),
     ],
     swiftLanguageVersions: [.v5]
