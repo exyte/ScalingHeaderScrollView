79dc66156e9bbca278f33d40e3d3a1555b8ae755
AlisaMylnikova <alisa.mylnikova@exyte.com>
2022-09-28 12:18:57 +0800

    Use ios 14 as deployment target

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ScalingHeaderScrollView",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
