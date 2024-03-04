458d79123dbead7253d586ecff4cbbc923ec356d
lashlyn <vat.13@list.ru>
2024-03-04 13:16:37 +0700

    fix issue content offset + sheet presentation

--- Source/ScalingHeaderScrollView.swift
+++ Source/ScalingHeaderScrollView.swift
@@ -366,7 +366,7 @@ public struct ScalingHeaderScrollView<Header: View, Content: View>: View {
     }
 
     private func getGeometryReaderVsScrollView(scrollGeometry: GeometryProxy, globalGeometry: GeometryProxy) -> CGFloat {
-        getScrollOffset() - scrollGeometry.frame(in: .global).minY + globalGeometry.frame(in: .global).minY
+        getScrollOffset() - scrollGeometry.frame(in: .global).minY - globalGeometry.frame(in: .global).minY
     }
 
     private func getOffsetForHeader() -> CGFloat {
